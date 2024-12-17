9f871363a32e37e9509016e149c33e10ca7c8752
ArijAguel <arij.aguel@insat.ucar.tn>
2024-12-17 14:11:36 +0100

    data preparation

---
+++ data-preparation.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+
+
+# Charger les données
+data = pd.read_csv('reviews.csv')  # Ou  pd.read_excel('reviews.xlsx)
+print(data.head())
+
+# Supprimer les lignes vides
+data.dropna(inplace=True)
+print(data.info())
+
+
+data = data[['title', 'rating', 'content']]
+
+
+# Télécharger les stopwords
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+# Initialiser lemmatizer
+lemmatizer = WordNetLemmatizer()
+
+def clean_text(text):
+    text = text.lower()  # Convertir en minuscules
+    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)  # Supprimer les URL
+    text = re.sub(r'\d+', '', text)  # Supprimer les chiffres
+    text = re.sub(r'[^\w\s]', '', text)  # Supprimer les caractères spéciaux
+    text = ' '.join([lemmatizer.lemmatize(word) for word in text.split()
+                     if word not in stopwords.words('english')])  # Retirer les stopwords
+    return text
+
+data['cleaned_content'] = data['content'].apply(clean_text)
+print(data.head())
+
+
+# Exporter le fichier nettoyé au format CSV
+data.to_csv('cleaned_reviews.csv', index=False, encoding='utf-8')
+print("Les données nettoyées ont été exportées dans 'cleaned_reviews.csv'")
+
